d46738087911f41dd9b094d4c6859f95d0540736
Ariane2023 <anne.de.abreu@gmail.com>
2023-06-22 15:42:07 -0400

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'Logo do Site Técnico 24h')
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  
+  
+}
